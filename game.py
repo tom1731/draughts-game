85a28d67225ad00307ac611bf86dc4a09cf5e288
tom <tom.desire17@gmail.com>
2021-08-12 19:18:46 +0200

    moving pawn

--- game.py
+++ game.py
@@ -29,6 +29,7 @@ class Game:
 
 
 class Selection:
+
     def __init__(self, origin, width, heigh):
         self.origin = origin
         self.width = width
@@ -58,10 +59,16 @@ class Selection:
                 print(i)
                 # self.selection = None
         else:
-            self.selection.move(pos)
-            self.selection = None
-
-
+            new_selection = False
+            for pawn in board.list_pawns:
+                if pawn.rect.collidepoint(pos):
+                    self.selection = pawn
+                    new_selection = True
+            if new_selection is False:
+                for case in board.list_cases:
+                    if case.collidepoint(pos):
+                        self.selection.move(case.center)
+                        self.selection = None
 
 
 class Board:
@@ -69,8 +76,10 @@ class Board:
     def __init__(self):
         self.board = pygame.image.load('data/images/board.jpg').convert()
         self.origin = (41, 41)
+        self.dim = (52, 52)
         self.width = 52.6
         self.heigh = 51.9
+        self.list_cases = []
         self.list_pawns = []
         self.grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -85,44 +94,54 @@ class Board:
         self.setup()
 
     def setup(self):
-        pos_white = [(9, 0), (9, 2), (9, 4), (9, 6), (9, 8),
-                     (8, 1), (8, 3), (8, 5), (8, 7), (8, 9),
-                     (7, 0), (7, 2), (7, 4), (7, 6), (7, 8),
-                     (6, 1), (6, 3), (6, 5), (6, 7), (6, 9)]
 
-        pos_black = [(0, 1), (0, 3), (0, 5), (0, 7), (0, 9),
-                     (1, 0), (1, 2), (1, 4), (1, 6), (1, 8),
-                     (2, 1), (2, 3), (2, 5), (2, 7), (2, 9),
-                     (3, 0), (3, 2), (3, 4), (3, 6), (3, 8)]
+        pos_white = list(range(30, 50))
+
+        pos_black = list(range(20))
+
+        origin = self.origin
+
+        for row in range(10):
+            if row % 2 == 0:
+                origin = (origin[0] - self.dim[0], origin[1])
+            else:
+                origin = (origin[0] - self.dim[0] * 2, origin[1])
+
+            for col in range(5):
+                origin = (origin[0] + self.dim[0] * 2, origin[1])
+                self.list_cases.append(pygame.Rect(origin, self.dim))
+            origin = (self.origin[0], origin[1] + self.dim[1])
 
         for pos in pos_white:
-            self.list_pawns.append(Pawn('white', pos, self.origin))
+            self.list_pawns.append(Pawn('white', self.list_cases[pos]))
 
         for pos in pos_black:
-            self.list_pawns.append(Pawn('black', pos, self.origin))
+            self.list_pawns.append(Pawn('black', self.list_cases[pos]))
 
     def display(self, screen):
+
         screen.blit(self.board, (0, 0))
+
+        for box in self.list_cases:
+            pygame.draw.rect(screen, (255, 0 , 0), box)
+
         for pawn in self.list_pawns:
             pawn.display(screen)
 
 
 class Pawn:
 
-    def __init__(self, color, pos, origin):
+    def __init__(self, color, case):
         if color == 'white':
             self.sprite = pygame.image.load('data/images/white.png').convert()
         elif color == 'black':
             self.sprite = pygame.image.load('data/images/black.png').convert()
         self.sprite.set_colorkey([0, 0, 255])
 
-        self.pos = pos
-        self.board_origin = origin
-        self.x = self.board_origin[0] + self.pos[1] * 53
-        self.y = self.board_origin[1] + self.pos[0] * 52
-
+        self.rect = self.sprite.get_rect(center=case.center)
+        self.x = self.rect.center[0] - self.rect.w / 2
+        self.y = self.rect.center[1] - self.rect.h / 2
         self.type = 'pawn'
-        self.rect = self.sprite.get_rect(topleft=(self.x, self.y))
 
     def display(self, screen):
         screen.blit(self.sprite, (self.x, self.y))
@@ -131,9 +150,9 @@ class Pawn:
         pass
 
     def move(self, pos):
-        self.x = pos[0] - 26
-        self.y = pos[1] - 26
-        self.rect.update(self.x, self.y, 52, 52 )
+        self.x = pos[0] - self.rect.w / 2
+        self.y = pos[1] - self.rect.h / 2
+        self.rect.update(self.x, self.y, self.rect.w, self.rect.h)
 
     def kill(self):
         pass
