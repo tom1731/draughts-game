62cafbc3935828e6ad2d1ef3d73321cba917619e
tom <tom.desire17@gmail.com>
2021-08-17 16:01:17 +0200

    valid move

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ class Game:
         pygame.display.set_caption('draughts game')
 
     def run(self):
+        active_player = 'white'
         running = True
         board = Board()
         selection = Selection(board.origin, board.width, board.heigh)
@@ -21,7 +22,11 @@ class Game:
                         running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
-                        selection.click(event.pos, board)
+                        if selection.click(event.pos, board, active_player) == 'moved':
+                            if active_player == 'white':
+                                active_player = 'black'
+                            else:
+                                active_player = 'white'
 
             board.display(self.screen)
             selection.display(self.screen)
@@ -44,7 +49,34 @@ class Selection:
                              (self.selection.x, self.selection.y, self.width, self.heigh),
                              width=3)
 
-    def click(self, pos, board):
+    def check_move(self, selection, pos, active_player):
+
+        for case in board.list_cases:
+            if case.collidepoint(pos):
+                break
+            print('not a case')
+            return 'not a case'
+
+        if active_player == 'white':
+            vects = [(-1, -1), (1, -1)]
+        else:
+            vects = [(-1, 1), (1, 1)]
+
+        center = selection.rect.center
+        for point in vects:
+            if case.collidepoint(center[0] + vects[0] * board.dim[0],
+                                 center[1] + vects[1] * board.dim[1]) is False:
+                print('invalid case')
+                return 'invalid case'
+
+        for pawn in board.list_pawns:
+            if pawn.rect.collidepoint(pos):
+                print('pawn already here')
+                return 'pawn already here'
+
+
+
+    def click(self, pos, board, active_player):
         # self.pos = (
         #     (pos[0] - self.origin[0]) // self.width * self.width + self.origin[0],
         #     (pos[1] - self.origin[1]) // self.heigh * self.heigh + self.origin[1]
@@ -53,8 +85,9 @@ class Selection:
             i = 0
             for pawn in board.list_pawns:
                 if pawn.rect.collidepoint(pos):
-                    self.selection = pawn
-                    break
+                    if pawn.color == active_player:
+                        self.selection = pawn
+                        break
                 i += 1
                 print(i)
                 # self.selection = None
@@ -62,13 +95,91 @@ class Selection:
             new_selection = False
             for pawn in board.list_pawns:
                 if pawn.rect.collidepoint(pos):
-                    self.selection = pawn
-                    new_selection = True
+                    if pawn.color == active_player:
+                        self.selection = pawn
+                        new_selection = True
+                    else:
+                        return
+
             if new_selection is False:
                 for case in board.list_cases:
                     if case.collidepoint(pos):
-                        self.selection.move(case.center)
-                        self.selection = None
+                        if pawn.type == 'pawn':
+                            if active_player == 'white':
+
+                                # white left moving
+                                if case.collidepoint(self.selection.rect.center[0] - board.dim[0],
+                                                     self.selection.rect.center[1] - board.dim[1]):
+                                    self.selection.move(case.center)
+                                    self.selection = None
+                                    return 'moved'
+
+                                # white right moving
+                                if case.collidepoint(self.selection.rect.center[0] + board.dim[0],
+                                                     self.selection.rect.center[1] - board.dim[1]):
+                                    self.selection.move(case.center)
+                                    self.selection = None
+                                    return 'moved'
+
+                                # white left eating
+                                if case.collidepoint(self.selection.rect.center[0] - 2 * board.dim[0],
+                                                     self.selection.rect.center[1] - 2 * board.dim[1]):
+                                    for pawn in board.list_pawns:
+                                        if pawn.color != active_player:
+                                            if pawn.rect.collidepoint(self.selection.rect.center[0] - board.dim[0],
+                                                                 self.selection.rect.center[1] - board.dim[1]):
+                                                self.selection.move(case.center)
+                                                self.selection = None
+                                                return 'moved'
+
+                                # white right eating
+                                if case.collidepoint(self.selection.rect.center[0] + 2 * board.dim[0],
+                                                     self.selection.rect.center[1] - 2 * board.dim[1]):
+                                    for pawn in board.list_pawns:
+                                        if pawn.color != active_player:
+                                            if pawn.rect.collidepoint(self.selection.rect.center[0] + board.dim[0],
+                                                                 self.selection.rect.center[1] - board.dim[1]):
+                                                self.selection.move(case.center)
+                                                self.selection = None
+                                                return 'moved'
+
+                            else:
+
+                                # black left moving
+                                if case.collidepoint(self.selection.rect.center[0] - board.dim[0],
+                                                     self.selection.rect.center[1] + board.dim[1]):
+                                    self.selection.move(case.center)
+                                    self.selection = None
+                                    return 'moved'
+
+                                # black right moving
+                                if case.collidepoint(self.selection.rect.center[0] + board.dim[0],
+                                                     self.selection.rect.center[1] + board.dim[1]):
+                                    self.selection.move(case.center)
+                                    self.selection = None
+                                    return 'moved'
+
+                                # black left eating
+                                if case.collidepoint(self.selection.rect.center[0] - 2 * board.dim[0],
+                                                     self.selection.rect.center[1] + 2 * board.dim[1]):
+                                    for pawn in board.list_pawns:
+                                        if pawn.color != active_player:
+                                            if pawn.rect.collidepoint(self.selection.rect.center[0] - board.dim[0],
+                                                                      self.selection.rect.center[1] + board.dim[1]):
+                                                self.selection.move(case.center)
+                                                self.selection = None
+                                                return 'moved'
+
+                                # black right eating
+                                if case.collidepoint(self.selection.rect.center[0] + 2 * board.dim[0],
+                                                     self.selection.rect.center[1] + 2 * board.dim[1]):
+                                    for pawn in board.list_pawns:
+                                        if pawn.color != active_player:
+                                            if pawn.rect.collidepoint(self.selection.rect.center[0] + board.dim[0],
+                                                                      self.selection.rect.center[1] + board.dim[1]):
+                                                self.selection.move(case.center)
+                                                self.selection = None
+                                                return 'moved'
 
 
 class Board:
@@ -142,6 +253,7 @@ class Pawn:
         self.x = self.rect.center[0] - self.rect.w / 2
         self.y = self.rect.center[1] - self.rect.h / 2
         self.type = 'pawn'
+        self.color = color
 
     def display(self, screen):
         screen.blit(self.sprite, (self.x, self.y))
